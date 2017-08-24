a2569ade849c0491ab64686992be3432b0bb59a0
hardikchoudhary <hardik.choudhary123@gmail.com>
2017-08-24 12:10:29 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-(function () {
-    "use strict";
-    exports.apiKey = process.env.GOOGLE_PLACES_API_KEY || "AIzaSyACurvXGgbQqG6fSe4-P1sOcEtwmyhXoTM";
-    exports.outputFormat = process.env.GOOGLE_PLACES_OUTPUT_FORMAT || "json";
-
-})();
